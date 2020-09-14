fbe8f847fad975475bbc7a307e182f483d1c26a0
TanvirRobbany <tanvirahmed80151@gmail.com>
2020-09-14 23:53:39 +0600

    Button Resolved

--- src/components/container/Admin/AdminPanel.js
+++ src/components/container/Admin/AdminPanel.js
@@ -12,16 +12,16 @@ const AdminPanel = () => {
             <Fragment>
                 <div className="container">
                     <div className="row" style={{ height: "700px" }}>
-                        <div className="col s12 m12 l3 grey lighten-2" style={{ height: "100%", borderRadius: "10px", display: "flex", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
+                        <div className="col s12 m12 l4 grey lighten-2" style={{ height: "100%", borderRadius: "10px", display: "flex", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
                             <h3 >Admin Panel</h3>
                             <a href="/admindashboard" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px" }}>Dashboard</a>
                             <a href="/adminaddbook" className="waves-effect waves-light btn-large blue-grey black-text lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px" }}>Add Book</a>
-                            <a href="/admincategories" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px" }}>Book Categories</a>
+                            <a href="/admincategories" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px",padding: "0px 0px 0px 0px", borderRadius: "10px" }}>Book Categories</a>
                             <a className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px" }}>Book Borrowed</a>
                             <a className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px" }}>Logout</a>
                         </div>
                         <Switch>
-                            <div className="col s12 m12 l9" style={{ marginTop: "10px" }}>
+                            <div className="col s12 m12 l8" style={{ marginTop: "10px" }}>
                                 <Route exact path="/admindashboard" component={Dashboard} />
                                 <Route exact path="/adminaddbook" component={AddBook} />
                                 <Route exact path="/admincategories" component={Categories} />
