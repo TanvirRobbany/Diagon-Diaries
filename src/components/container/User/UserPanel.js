e5778e26f75e3c1504247cd6e48d421f1743cac4
TanvirRobbany <tanvirahmed80151@gmail.com>
2020-09-18 22:29:45 +0600

    Admin Changed To User

--- src/components/container/User/UserPanel.js
+++ src/components/container/User/UserPanel.js
@@ -11,7 +11,7 @@ const UserPanel = () => {
                 <div className="container" style={{marginBottom: "25px"}}>
                     <div className="row" style={{ height: "700px",}}>
                         <div className="col s12 m12 l3 grey lighten-2" style={{ height: "100%", borderRadius: "10px", display: "flex", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
-                            <h3 >Admin Panel</h3>
+                            <h3 >User Panel</h3>
                             <Link to="/userdashboard" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px", padding: "0" }}>Dashboard</Link>
                             <Link to="/usercategories" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px", padding: "0" }}>Book Categories</Link>
                             <Link to="/userbookborrowed" className="waves-effect waves-light btn-large black-text blue-grey lighten-2" style={{ width: "90%", fontSize: "20px", borderRadius: "10px", padding: "0" }}>Book Borrowed</Link>
