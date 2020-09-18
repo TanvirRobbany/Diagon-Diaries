7bac0f67b744e61f197fe073513b62f3a9d27e71
TanvirRobbany <tanvirahmed80151@gmail.com>
2020-09-18 23:34:21 +0600

    Add Book Modified

--- src/components/container/Admin/AddBook.js
+++ src/components/container/Admin/AddBook.js
@@ -3,81 +3,91 @@ import { Link } from "react-router-dom";
 import M from "materialize-css";
 
 const AddBook = () => {
-  useEffect(() => {
-    M.AutoInit();
-  }, []);
-  return (
-    <div className='container'>
-      <form action=''>
-        <h3>Add New Book</h3>
-        <div className='input-field'>
-          <input type='text' id='booktitle' />
-          <label
-            className='white-text'
-            style={{ fontSize: "20px" }}
-            htmlFor='booktitle'
-          >
-            Book Title:
+
+    useEffect(() => {
+        M.AutoInit();
+    }, []);
+
+    return (
+        <div className='container'>
+            <form action=''>
+                <h3>Add New Book</h3>
+                <div className='input-field'>
+                    <input type='text' id='booktitle' />
+                    <label
+                        className='white-text'
+                        style={{ fontSize: "20px" }}
+                        htmlFor='booktitle'
+                    >
+                        Book Title:
           </label>
-        </div>
-        <div className='input-field'>
-          <input type='text' id='authorname' />
-          <label
-            className='white-text'
-            style={{ fontSize: "20px" }}
-            htmlFor='authorname'
-          >
-            Author(s) Name(s):
+                </div>
+                <div className='input-field'>
+                    <input type='text' id='authorname' />
+                    <label
+                        className='white-text'
+                        style={{ fontSize: "20px" }}
+                        htmlFor='authorname'
+                    >
+                        Author(s) Name(s):
           </label>
-        </div>
-        <div className='input-field'>
-          <input type='text' id='bookcode' />
-          <label
-            className='white-text'
-            style={{ fontSize: "20px" }}
-            htmlFor='bookcode'
-          >
-            Book Code:
+                </div>
+                <div className='input-field'>
+                    <input type='text' id='bookcode' />
+                    <label
+                        className='white-text'
+                        style={{ fontSize: "20px" }}
+                        htmlFor='bookcode'
+                    >
+                        Book Code:
+                    </label>
+                </div>
+                {/* <div className='input-field'>
+                    <input type='text' id='category' />
+                    <label
+                        className='white-text'
+                        style={{ fontSize: "20px" }}
+                        htmlFor='category'
+                    >
+                        Category:
           </label>
-        </div>
-        <div className='input-field'>
-          <input type='text' id='category' />
-          <label
-            className='white-text'
-            style={{ fontSize: "20px" }}
-            htmlFor='category'
-          >
-            Category:
+                </div> */}
+                <div class='input-field'>
+                    <select class='browser-deafault'>
+                        <option>Choose Category</option>
+                        <option value='1'>CSE</option>
+                        <option value='2'>EEE</option>
+                        <option value='3'>Textile</option>
+                        <option value='4'>Architecture</option>
+                        <option value='5'>Pharmacy</option>
+                        <option value='6'>BBA</option>
+                        <option value='7'>Economics</option>
+                        <option value='8'>English</option>
+                        <option value='9'>Bengali</option>
+                        <option value='10'>Religious</option>
+                        <option value='11'>Magazine</option>
+                        <option value='12'>Journal</option>
+                    </select>
+                </div>
+                <div className='input-field'>
+                    <input type='text' id='quantity' />
+                    <label
+                        className='white-text'
+                        style={{ fontSize: "20px" }}
+                        htmlFor='quantity'
+                    >
+                        Quantity:
           </label>
-        </div>
-        <div class='input-field'>
-          <select class='browser-deafault'>
-            <option>Choose your option</option>
-            <option value='1'>Option 1</option>
-            <option value='2'>Option 2</option>
-            <option value='3'>Option 3</option>
-          </select>
-          <label>Materialize Select</label>
-        </div>
-        <div className='input-field'>
-          <input type='text' id='quantity' />
-          <label
-            className='white-text'
-            style={{ fontSize: "20px" }}
-            htmlFor='quantity'
-          >
-            Quantity:
-          </label>
-        </div>
-        <Link
-          class='waves-effect waves-light btn-large green lighten-2'
-          style={{ width: "50%", height: "70%", borderRadius: "10px" }}
-        >
-          Add Book
+                </div>
+                <Link
+                    class='waves-effect waves-light btn-large green lighten-2'
+                    style={{ width: "50%", height: "70%", borderRadius: "10px" }}
+                >
+                    Add Book
         </Link>
-      </form>
-    </div>
-  );
+            </form>
+        </div>
+    );
 };
 
 export default AddBook;
